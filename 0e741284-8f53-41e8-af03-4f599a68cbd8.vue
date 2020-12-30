ce3afd037a0e6e35f0b1ad889b2b3579c2ec8d9e
Batuhan Yenidünya <44680817+batuhanyndny@users.noreply.github.com>
2020-12-31 01:32:37 +0300

    BatuhanYenidunya uploaded for review at 12/31/2020

---
+++ 0e741284-8f53-41e8-af03-4f599a68cbd8.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="droppable block h-half bg-white">
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Drop",
+}
+</script>
+
+<style scoped>
+.h-half{
+    height: 50vh;
+}
+</style>
